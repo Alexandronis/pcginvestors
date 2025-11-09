46eb5d2421053c5765bb149cdc2e69907d1f1390
Oleksandr Speranskyi <sasha.speransky@gmail.com>
2025-11-09 16:37:04 +0200

    fixed styles for slider.

--- src/component/Quotes.js
+++ src/component/Quotes.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-var settings = {
+const settings = {
     dots: true,
     infinite: true,
     slidesToShow: 1,
