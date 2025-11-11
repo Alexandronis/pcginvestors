8d272c0081273541adf0a19da2e2ec80365dcdd6
Oleksandr Speranskyi <sasha.speransky@gmail.com>
2025-11-12 01:22:23 +0200

    refactored Chart.js

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,13 +1,20 @@
 import React, { useState } from "react";
-const charityData = require("../data/charity.json");
+import charityData from "../data/charity.json";
 
-function Chart() {
-  const [onHover, setonHover] = useState(9);
+const Chart = () => {
+  const [onHover, setOnHover] = useState(9);
+  const handleMouseEnter = (index) => {
+    onHover === index + 1 ? setOnHover(0) : setOnHover(index + 1);
+  };
+
+  const handleMouseLeave = () => {
+    setOnHover(0);
+  };
 
   return (
     <div className="menu-section">
       <span className="middle-logo">
-        <img src="/pcg-logo.png" alt="pcg Logo" />
+        <img src="/pcg-logo.png" alt="pcg Logo" loading="lazy" />
       </span>
       <span className="half-circle"></span>
       <div className="circular-wrapper">
@@ -22,59 +29,52 @@ function Chart() {
       </div>
       <div className="item-wrapper">
         <ul>
-          {charityData.map((data, index) => {
-            return (
-              <li
-                className="icon-circle"
-                style={{
-                  transform: `rotateZ(calc((360deg / 7) * ${data.id + 1}))`,
-                }}
-                key={data.id}
+          {charityData.map((data, index) => (
+            <li
+              className="icon-circle"
+              style={{
+                transform: `rotateZ(calc((360deg / 7) * ${data.id + 1}))`,
+              }}
+              key={data.id}
+            >
+              <span
+                className={
+                  onHover === index + 1
+                    ? `white-circle logo-circle${index} active`
+                    : onHover === 0
+                      ? `white-circle logo-circle${index}`
+                      : `white-circle logo-circle${index} none-active`
+                }
+                onMouseEnter={() => handleMouseEnter(index)}
+                onMouseLeave={handleMouseLeave}
               >
-                <span
+                <img
+                  className={onHover === data.id + 1 ? "icon-none" : "icon-image"}
+                  src={data.logoRound}
+                  alt={data.title}
+                  loading="lazy"
+                />
+                <div
                   className={
-                    onHover === index + 1
-                      ? `white-circle logo-circle${index} active`
-                      : onHover === 0
-                        ? `white-circle logo-circle${index}`
-                        : `white-circle logo-circle${index} none-active`
+                    onHover === data.id + 1
+                      ? `card-active back${index}`
+                      : "card-hide"
                   }
-                  onMouseEnter={() => {
-                    onHover === index + 1
-                      ? setonHover(0)
-                      : setonHover(index + 1);
-                  }}
-                  onMouseLeave={() => setonHover(0)}
                 >
-                  <img
-                    className={
-                      onHover === data.id + 1 ? "icon-none" : "icon-image"
-                    }
-                    src={data.logoRound}
-                    alt={data.title}
-                  />
-                  <div
-                    className={
-                      onHover === data.id + 1
-                        ? `card-active back${index}`
-                        : "card-hide"
-                    }
-                  >
-                    <p>{data.description}</p>
-                    <p className="right-align">
-                      <a href={data.webLink} target="_blank" rel="noreferrer">
-                        See More &gt;
-                      </a>
-                    </p>
-                  </div>
-                </span>
-              </li>
-            );
-          })}
+                  <p>{data.description}</p>
+                  <p className="right-align">
+                    <a href={data.webLink} target="_blank" rel="noreferrer">
+                      See More &gt;
+                    </a>
+                  </p>
+                </div>
+              </span>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
   );
-}
+};
 
 export default Chart;
