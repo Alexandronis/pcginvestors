afb960ab3d14f14129dcdbb54aefda8550f30257
Oleksandr Speranskyi <sasha.speransky@gmail.com>
2025-11-11 22:33:01 +0200

    refactored 404.js

--- src/404.js
+++ src/404.js
@@ -1,18 +1,16 @@
-import React from "react";
+import React, { useEffect } from 'react';
 
-const NotFoundPage = props => {
-  if (props.location.pathname === "/404") {
-    document.body.classList.remove("404-page");
-  }
+const NotFoundPage = () => {
+  useEffect(() => {
+    document.body.classList.remove('404-page');
+  }, []);
 
   return (
-    <div>
-      <div className="pagenotfound-wrapper">
-        <img className="fourOfour" src="/404_page/404_image.png" alt="404" />
-        <h2>We couldn&apos;t find what you were looking for</h2>
-        <img src="/404_page/pcg_logo.png" alt="404" />
-        <a href="/">Home</a>
-      </div>
+    <div className="pagenotfound-wrapper">
+      <img className="fourOfour" src="/404_page/404_image.png" alt="404" />
+      <h2>We couldn&apos;t find what you were looking for</h2>
+      <img src="/404_page/pcg_logo.png" alt="404" />
+      <a href="/">Home</a>
     </div>
   );
 };
