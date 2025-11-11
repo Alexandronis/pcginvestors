a0c79c96b681532fb2ef0f1d95c9ca05578e4eed
Oleksandr Speranskyi <sasha.speransky@gmail.com>
2025-11-11 22:58:44 +0200

    refactored Portfolio2/PortfolioItem.js

--- src/components/Portfolio/Portfolio2/PortfolioItem.js
+++ src/components/Portfolio/Portfolio2/PortfolioItem.js
@@ -1,64 +1,62 @@
 import React, { useRef } from "react";
 
-const PortfolioItem = props => {
+const PortfolioItem = ({ data, index, opened, setOpen, ScrollWhenClick }) => {
   const dataCard = useRef(null);
 
   const handleClick = () => {
-    if (props.opened) {
-      props.setOpen(null);
+    if (opened) {
+      setOpen(null);
     } else {
-      props.setOpen(props.index);
-      props.ScrollWhenClick(props.index);
+      setOpen(index);
+      ScrollWhenClick(index);
     }
   };
+
   return (
     <div className="Item_wrapper">
-      <div
-        className={
-          props.opened ? "dataCard_wrapper opened" : "dataCard_wrapper"
-        }
-      >
+      <div className={opened ? "dataCard_wrapper opened" : "dataCard_wrapper"}>
         <div ref={dataCard} className="dataCard">
           <div className="title_section">
-            <h3>{props.data.company}</h3>
-
-            <a target="_BLANK" rel="noopener noreferrer" href={props.data.url}>
-              Visit Website{" "}
+            <h3>{data.company}</h3>
+            <a target="_blank" rel="noopener noreferrer" href={data.url}>
+              Visit Website
             </a>
           </div>
 
           <div className="desc_section">
             <span>&quot;</span>
-            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{props.data.description}</p>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{data.description}</p>
             <span>&quot;</span>
           </div>
+
           <div className="name_section">
-            <h4>{props.data.name}</h4>
-            <h4>{props.data.designation}</h4>
-            <h4>{props.data.company}</h4>
+            <h4>{data.name}</h4>
+            <h4>{data.designation}</h4>
+            <h4>{data.company}</h4>
           </div>
         </div>
       </div>
+
       <div
         role="button"
         tabIndex={0}
-        className={props.opened ? "portfolioClose opened" : "portfolioClose"}
+        className={opened ? "portfolioClose opened" : "portfolioClose"}
         onClick={handleClick}
-        onKeyPress={(e) => e.key === "Enter" && handleClick(e)}
-
+        onKeyPress={(e) => e.key === "Enter" && handleClick()}
       >
         <div className="icon">
           <span className="bar"></span>
         </div>
       </div>
+
       <div
         role="button"
         tabIndex={0}
-        className={props.opened ? "portfolioItem opened" : "portfolioItem"}
+        className={opened ? "portfolioItem opened" : "portfolioItem"}
         onClick={handleClick}
-        onKeyPress={(e) => e.key === "Enter" && handleClick(e)}
+        onKeyPress={(e) => e.key === "Enter" && handleClick()}
       >
-        <img alt={props.data.alt} src={props.data.image} />
+        <img alt={data.alt} src={data.image} loading="lazy" />
       </div>
     </div>
   );
