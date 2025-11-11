b428c3d05d540b5fdab5a7c77fae71e702008927
Oleksandr Speranskyi <sasha.speransky@gmail.com>
2025-11-11 22:53:39 +0200

    refactored Portfolio1/Portfolio.js

--- src/components/Portfolio/Portfolio1/Portfolio.js
+++ src/components/Portfolio/Portfolio1/Portfolio.js
@@ -1,20 +1,18 @@
 import React, { useCallback, useState, useEffect, useRef } from "react";
-
 import PortfolioItem from "./PortfolioItem";
-
-const Data = require("../../../data/portfolio.json");
+import Data from "../../../data/portfolio.json";
 
 let scrollDir = 1;
 let scroller = null;
 
 const Portfolio = () => {
   const slider = useRef(null);
-  const [OpenedItem, setOpenedItem] = useState(null);
+  const [openedItem, setOpenedItem] = useState(null);
 
-  // scroll the Slider
   const scrollSlider = useCallback(() => {
-    if (slider.current && OpenedItem === null) {
+    if (slider.current && openedItem === null) {
       slider.current.scroll((slider.current.scrollLeft += scrollDir), 0);
+
       if (slider.current.scrollLeft === slider.current.scrollWidth - slider.current.clientWidth) {
         scrollDir = -1;
       }
@@ -22,10 +20,10 @@ const Portfolio = () => {
         scrollDir = 1;
       }
     }
-  }, [OpenedItem]);
+  }, [openedItem]);
 
-  const ScrollWhenClick = i => {
-    const amount = Math.floor(i / 3) * 170 - window.innerWidth / 2 + 380;
+  const scrollWhenClick = (index) => {
+    const amount = Math.floor(index / 3) * 170 - window.innerWidth / 2 + 380;
     slider.current.scroll(amount, 0);
   };
 
@@ -39,39 +37,25 @@ const Portfolio = () => {
       <h1 className="title">PCG Investment Portfolio</h1>
 
       <div
-        onMouseOver={() => {
-          clearInterval(scroller);
-        }}
-        onMouseLeave={() => {
-          scroller = setInterval(scrollSlider, 30);
-        }}
-        onFocus={() => {
-          clearInterval(scroller);
-        }}
-        onBlur={() => {
-          scroller = setInterval(scrollSlider, 30);
-        }}
         ref={slider}
         className="prtfolio_slider"
+        onMouseOver={() => clearInterval(scroller)}
+        onMouseLeave={() => (scroller = setInterval(scrollSlider, 30))}
+        onFocus={() => clearInterval(scroller)}
+        onBlur={() => (scroller = setInterval(scrollSlider, 30))}
       >
-        <div
-          className={
-            OpenedItem ? "prtfolio_container pause" : "prtfolio_container"
-          }
-        >
-          {Data.map((data, i) => {
-            return (
-              <PortfolioItem
-                data={data}
-                index={i}
-                key={i}
-                opened={i === OpenedItem}
-                openedId={OpenedItem}
-                setOpen={setOpenedItem}
-                ScrollWhenClick={ScrollWhenClick}
-              />
-            );
-          })}
+        <div className={openedItem ? "prtfolio_container pause" : "prtfolio_container"}>
+          {Data.map((data, i) => (
+            <PortfolioItem
+              key={i}
+              data={data}
+              index={i}
+              opened={i === openedItem}
+              openedId={openedItem}
+              setOpen={setOpenedItem}
+              ScrollWhenClick={scrollWhenClick}
+            />
+          ))}
         </div>
       </div>
     </div>
